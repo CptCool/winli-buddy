e040c59d6bcbec2891169235f84394abfadb57f8
CptCool <florian.schwanse@rwth-aachen.de>
2019-03-03 18:11:51 +0100

    Datei hochgeladen

---
+++ WinLiBuddy.py
@@ -0,0 +1,35 @@
+import pyperclip
+# import re
+
+rin = pyperclip.paste()
+# rin is an input string variable I am using inside this script
+rout = None
+# rout is the output string variable I am unsing inside this script. The start has to be empty
+print ("This input is detected ", rin)
+
+if rin.find("/")
+    print "Path in Linux format detected"
+    liste = rin.split("/")
+    rout = "\\strongbow" # start value, works only on strongbow server for moment. Has to changed to a variable which can be detemined inside a setting file
+    for n in len(liste)
+        if liste[n] = "/"
+            rout = (rout, "\")
+        else
+            rout = (rout, liste[n])
+    pyperclip.copy(r)
+    print ("the path ", rout, " has been copied to clipboard. This is Windows Format")
+
+if rin.find("\"")
+    print "Path in Windows format detected"
+    liste = r.split("\")
+    rout = None #maybe this makes no sense
+    rout = "shares/"  # start value, works only on strongbow server for moment. Has to changed to a variable which can be detemined inside a setting file
+    for n in len(liste)
+        if liste[n] = "\"
+            rout = (rout, "/")
+        else
+            rout = (rout, liste[n])
+    pyperclip.copy(r)
+    print ("the path ", rout, " has been copied to clipboard. This is Linux Format")
+else:
+    print "I could not detect anything"
